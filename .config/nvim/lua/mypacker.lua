c0708d94d541cce2f877b8d1c7d67da2f800c1ff
alfiankan <alfiankan19@gmail.com>
2022-12-30 06:59:35 +0700

    feat(colors): add colorshceme

--- .config/nvim/lua/mypacker.lua
+++ .config/nvim/lua/mypacker.lua
@@ -57,6 +57,14 @@ packer.startup(function()
   use 'xolox/vim-session'
   use 'sbdchd/neoformat'
 
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+        vim.cmd('colorscheme rose-pine')
+    end
+  })
+
   use {
     'nvim-treesitter/nvim-treesitter',
      run = ':TSUpdate'
