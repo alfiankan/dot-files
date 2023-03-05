d7c6049fe2cf877e334ae9c6e528f8c211f56ceb
alfiankan <alfiankan19@gmail.com>
2023-03-05 07:13:59 +0700

    Update mykeymaps.lua

--- .config/nvim/lua/mykeymaps.lua
+++ .config/nvim/lua/mykeymaps.lua
@@ -6,7 +6,8 @@ keymap("n", "<C-b>", ":NERDTreeToggle<cr>", { noremap = true, silent = true })
 keymap("n", "<C-t>", ":TagbarToggle<cr>", { noremap = true, silent = true })
 keymap("n", "<C-f>", ":Files<cr>", { noremap = true, silent = true })
 keymap("n", "<leader>f", ":Files<cr>", { noremap = true, silent = true })
-keymap("n", "T", ":Telescope command_palette<cr>", { noremap = true, silent = true })
+keymap("n", "K", ":Lspsaga hover_doc<cr>", { noremap = true, silent = true })
+keymap("n", "<leader>Space", ":Telescope command_palette<cr>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>ps", function ()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
